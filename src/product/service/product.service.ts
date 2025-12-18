cc34358a08c242f735a310c1997abbf8a948ddd4
Marcos santos <blackmaxsuke0220@gmail.com>
2025-12-18 14:19:01 -0300

    fix:conflits

--- src/product/service/product.service.ts
+++ src/product/service/product.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { Product } from "../entities/product.entity";
-import { Repository } from "typeorm";
+import { ILike, Repository } from "typeorm";
 
 @Injectable()
 export class ProductService {
     constructor(@InjectRepository(Product) private readonly productRepository: Repository<Product>) {}
+    
+
+    async findAll(): Promise<Product[]>{
+        return await this.productRepository.find()
+    }
+
+    async findById(id: number): Promise<Product>{
+        const product = await this.productRepository.findOne({
+            where: {
+                id,
+            },
+        });
+        if(!product)
+            throw new HttpException('Postagem não encontrada', HttpStatus.NOT_FOUND);
+
+        return product;
+    }
+
+    async findByName(name: string): Promise<Product[]>{
+        return await this.productRepository.find({
+            where: {
+                name: ILike(`%${name}%`)
+            },
+        })
+    }
+
+    async create(product: Product): Promise<Product>{
+        
+        return await this.productRepository.save(product)
+    }
+
+    /*async update(product: Product): Promise<Product>{
+        await this.findById(product.id);
+
+        await this.
+    }*/
 }
