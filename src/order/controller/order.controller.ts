c65390004e4b5644894457dcf8d38e695c26efd3
Gabriela Lima <gabriela.deabreulima@gmail.com>
2025-12-18 15:23:28 -0300

    feat: add CRUD for order and order item

--- src/order/controller/order.controller.ts
+++ src/order/controller/order.controller.ts
@@ -1,7 +1,38 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { OrderService } from "../service/order.service";
+import { Order } from "../entities/order.entity";
 
 @Controller('order')
 export class OrderController {
-    constructor(private readonly orderService: OrderService) {}
+    constructor(private readonly orderService: OrderService,) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Order[]> {
+    return this.orderService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Order> {
+    return this.orderService.findById(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() order: Order): Promise<Order> {
+    return this.orderService.create(order);
+  }
+
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() order: Order): Promise<Order> {
+    return this.orderService.update(order);
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number){
+    return this.orderService.delete(id);
+  }
 }
