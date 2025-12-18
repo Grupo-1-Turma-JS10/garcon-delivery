d0ee684419731ecc6fcdd723d293bd052265594d
Daniel Ribeiro <dann.ribeiroo@gmail.com>
2025-12-18 16:17:01 -0300

    feat: creates order items controller methods

--- src/order/controller/order-item.controller.ts
+++ src/order/controller/order-item.controller.ts
@@ -1,7 +1,38 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from "@nestjs/common";
 import { OrderItemService } from "../service/order-item.service";
+import { OrderItem } from "../entities/order-item.entity";
 
 @Controller('order-item')
 export class OrderItemController {
     constructor(private readonly orderItemService: OrderItemService) {}
+
+    @Post()
+    async create(@Body() orderItem: OrderItem) {
+        return await this.orderItemService.create(orderItem);
+    }
+
+    @Get()
+    async findAll() {
+        return await this.orderItemService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: string) {
+        return await this.orderItemService.findById(+id);
+    }
+
+    @Get('order/:orderId')
+    async findByOrder(@Param('orderId') orderId: string) {
+        return await this.orderItemService.findByOrder(+orderId);
+    }
+
+    @Patch(':id')
+    async update(@Param('id', ParseIntPipe) id: number, @Body() orderItem: OrderItem) {
+        return await this.orderItemService.update(id, orderItem);
+    }
+
+    @Delete(':id')
+    async delete(@Param('id', ParseIntPipe) id: number) {
+        return await this.orderItemService.delete(id);
+    }
 }
