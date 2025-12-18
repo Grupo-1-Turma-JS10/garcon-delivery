a562b99f05db5d8d275740e6cc678d4550137dc7
michael <michael.sf36@yahoo.com.br>
2025-12-18 15:41:50 -0300

    aplica stash

--- src/address/controller/address.controller.ts
+++ src/address/controller/address.controller.ts
@@ -1,7 +1,50 @@
-import { Controller } from "@nestjs/common";
-import { AddressService } from "../service/address.service";
+import {Body,Controller,Delete,Get,Param,Post,Put,Query,} from '@nestjs/common';
+import { AddressService } from '../service/address.service';
+import { Address } from '../entities/address.entity';
 
 @Controller('address')
 export class AddressController {
-    constructor(private readonly addressService: AddressService) {}
+  constructor(private readonly addressService: AddressService) {}
+
+  //  CREATE
+  // POST /address?userId=1
+  @Post()
+  create(
+    @Query('userId') userId: number,
+    @Body() address: Address,
+  ) {
+    return this.addressService.create(address, userId);
+  }
+
+  //  READ - todos
+  // GET /address
+  @Get()
+  findAll() {
+    return this.addressService.findAll();
+  }
+
+  //  READ - por id
+  // GET /address/1
+  @Get(':id')
+  findById(@Param('id') id: number) {
+    return this.addressService.findById(id);
+  }
+
+  //  UPDATE
+  // PUT /address/1?userId=1
+  @Put(':id')
+  update(
+    @Param('id') id: number,
+    @Query('userId') userId: number,
+    @Body() address: Address,
+  ) {
+    return this.addressService.update(id, address, userId);
+  }
+
+  //  DELETE
+  // DELETE /address/1
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.addressService.delete(id);
+  }
 }
